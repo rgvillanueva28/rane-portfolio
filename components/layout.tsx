58781849707f637913c20a63f75c686f06cc0653
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2022-07-19 00:25:22 +0800

    Added framer motion Furnished Homepage

--- components/layout.tsx
+++ components/layout.tsx
@@ -10,7 +10,7 @@ function Layout({ children }: Props) {
     <div className="flex flex-row bg-sky-100  text-sky-900 min-h-screen">
       {/* dark:text-white dark:bg-sky-900 */}
       <NavBar />
-      <main className="min-h-screen max-h-screen w-full bg-pattern">
+      <main className="min-h-screen max-h-screen w-full bg-pattern flex flex-col justify-center">
         {children}
       </main>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,98 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { motion } from "framer-motion";
 
 const Home: NextPage = () => {
+  const motionDiv = {
+    active: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 500,
+      },
+    },
+    inactive: { opacity: 0, y: -50 },
+  };
+
+  const motionRaneV = {
+    active: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: 1,
+        type: "spring",
+        stiffness: 1000,
+      },
+    },
+    inactive: { opacity: 0, x: 100 },
+  };
+
+  const motionButtons = {
+    active: {
+      y: [0, -10, 0],
+      transition: {
+        duration: 1,
+        ease: "easeInOut",
+        times: [0, 0.5, 1],
+        loop: Infinity,
+        repeatDelay: 0.2,
+      },
+    },
+    whileHover: {
+      y: 0,
+      transition: {
+        duration: 0.1,
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  const rgv = Array.from("Rane Villanueva.");
+
   return (
     <>
       <Head>
         <title>Rane Villanueva</title>
       </Head>
-      <div className="container mx-auto flex px-10 flex-col justify-center min-h-screen w-full">
+      <motion.div
+        initial="inactive"
+        animate="active"
+        variants={motionDiv}
+        className="container m-auto my-auto px-10 w-full"
+      >
         <div className="font-semibold text-lg">Hi there! I am</div>
-        <div className="text-6xl font-bold py-2">Rane Villanueva.</div>
+        <motion.div
+          initial="inactive"
+          animate="active"
+          variants={motionRaneV}
+          className="flex"
+        >
+          {rgv.map((rgv, index) =>
+            rgv === " " ? (
+              <div key={index} className="w-5"></div>
+            ) : (
+              <motion.div
+                key={index}
+                whileHover={{
+                  y: -10,
+                  transition: {
+                    type: "spring",
+                    stiffness: 1000,
+                  },
+                }}
+                whileTap={{
+                  y: 0,
+                  color: "#3b82f6",
+                }}
+                className="cursor-pointer text-6xl text-left font-bold"
+              >
+                {rgv}
+              </motion.div>
+            )
+          )}
+        </motion.div>
         <div className="gradient-text text-5xl font-bold py-2">
           Aspiring Developer.
         </div>
@@ -21,10 +104,15 @@ const Home: NextPage = () => {
           several projects. Check out the portfolio tab for more information
           about these projects.
         </div>
-        <button className="text-left px-6 h-12 bg-sky-100 border-2 border-sky-200 mr-auto mt-2 font-semibold hover:bg-sky-400 hover:border-sky-900">
+        <motion.button
+          animate="active"
+          whileHover="whileHover"
+          variants={motionButtons}
+          className="text-left px-6 h-12 bg-sky-100 border-2 border-sky-200 mr-auto mt-2 font-semibold hover:bg-sky-400 hover:border-sky-900"
+        >
           CONTACT ME
-        </button>
-      </div>
+        </motion.button>
+      </motion.div>
     </>
   );
 };
