9e61d6cba10b5b7bb178d23fafb8cb790bc58bb0
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2022-10-10 12:31:10 +0800

    SEO Optimization

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,24 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="theme-color" content="#0ea5e9" />
+          <meta
+            name="description"
+            content="Personal Website of Rane Villanueva."
+          />
+          <meta property="og:title" content="Rane Villanueva" />
+          <meta
+            property="og:description"
+            content="Personal Website of Rane Villanueva."
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://www.ranevillanueva.com/" />
+          <meta
+            property="og:image"
+            content="https://www.ranevillanueva.com/logo.png"
+          />
+          <link rel="apple-touch-icon" href="/logo.png"></link>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
@@ -12,7 +30,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
-        <body >
+        <body>
           <Main />
           <NextScript />
         </body>
